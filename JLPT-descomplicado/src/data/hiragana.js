9cd6679f660e79ddd62bc312f74832271334fd2e
Karla Oshikawa <karla_hara@hotmail.com>
2023-05-28 05:49:12 +0900

    data: hiragana list

---
+++ JLPT-descomplicado/src/data/hiragana.js
@@ -0,0 +1,834 @@
+const hiraganaList = [
+  {
+    a: {
+      letra: "a",
+      hiragana: "あ",
+      exemplos: [
+        {
+          palavra: "あさ",
+          traducao: "manhã",
+        },
+        {
+          palavra: "あめ",
+          traducao: "chuva",
+        },
+        {
+          palavra: "あかい",
+          traducao: "vermelho",
+        },
+      ],
+    },
+    i: {
+      letra: "i",
+      hiragana: "い",
+      exemplos: [
+        {
+          palavra: "いえ",
+          traducao: "casa",
+        },
+        {
+          palavra: "いぬ",
+          traducao: "cachorro",
+        },
+        {
+          palavra: "いちご",
+          traducao: "morango",
+        },
+      ],
+    },
+    u: {
+      letra: "u",
+      hiragana: "う",
+      exemplos: [
+        {
+          palavra: "うみ",
+          traducao: "mar",
+        },
+        {
+          palavra: "うさぎ",
+          traducao: "coelho",
+        },
+        {
+          palavra: "うた",
+          traducao: "música",
+        },
+      ],
+    },
+    e: {
+      letra: "e",
+      hiragana: "え",
+      exemplos: [
+        {
+          palavra: "えき",
+          traducao: "estação de trem",
+        },
+        {
+          palavra: "えんぴつ",
+          traducao: "lápis",
+        },
+        {
+          palavra: "えいご",
+          traducao: "inglês",
+        },
+      ],
+    },
+    o: {
+      letra: "o",
+      hiragana: "お",
+      exemplos: [
+        {
+          palavra: "おおきい",
+          traducao: "grande",
+        },
+        {
+          palavra: "おにぎり",
+          traducao: "bolinho de arroz",
+        },
+        {
+          palavra: "おとうさん",
+          traducao: "pai",
+        },
+      ],
+    },
+    ka: {
+      letra: "ka",
+      hiragana: "か",
+      exemplos: [
+        {
+          palavra: "かさ",
+          traducao: "guarda-chuva",
+        },
+        {
+          palavra: "かわ",
+          traducao: "rio",
+        },
+        {
+          palavra: "かいしゃ",
+          traducao: "empresa",
+        },
+      ],
+    },
+    ki: {
+      letra: "ki",
+      hiragana: "き",
+      exemplos: [
+        {
+          palavra: "きょう",
+          traducao: "hoje",
+        },
+        {
+          palavra: "きっさてん",
+          traducao: "cafeteria",
+        },
+        {
+          palavra: "きのこ",
+          traducao: "cogumelo",
+        },
+      ],
+    },
+    ku: {
+      letra: "ku",
+      hiragana: "く",
+      exemplos: [
+        {
+          palavra: "くるま",
+          traducao: "carro",
+        },
+        {
+          palavra: "くつ",
+          traducao: "sapato",
+        },
+        {
+          palavra: "くろ",
+          traducao: "preto",
+        },
+      ],
+    },
+    ke: {
+      letra: "ke",
+      hiragana: "け",
+      exemplos: [
+        {
+          palavra: "けんきゅう",
+          traducao: "pesquisa",
+        },
+        {
+          palavra: "けいさつ",
+          traducao: "polícia",
+        },
+        {
+          palavra: "けっこん",
+          traducao: "casamento",
+        },
+      ],
+    },
+    ko: {
+      letra: "ko",
+      hiragana: "こ",
+      exemplos: [
+        {
+          palavra: "こども",
+          traducao: "criança",
+        },
+        {
+          palavra: "こうえん",
+          traducao: "parque",
+        },
+        {
+          palavra: "こわい",
+          traducao: "assustador",
+        },
+      ],
+    },
+    sa: {
+      letra: "sa",
+      hiragana: "さ",
+      exemplos: [
+        {
+          palavra: "さくら",
+          traducao: "cerejeira",
+        },
+        {
+          palavra: "さかな",
+          traducao: "peixe",
+        },
+        {
+          palavra: "さく",
+          traducao: "flor",
+        },
+      ],
+    },
+    shi: {
+      letra: "shi",
+      hiragana: "し",
+      exemplos: [
+        {
+          palavra: "しお",
+          traducao: "sal",
+        },
+        {
+          palavra: "しごと",
+          traducao: "trabalho",
+        },
+        {
+          palavra: "した",
+          traducao: "abaixo",
+        },
+      ],
+    },
+    su: {
+      letra: "su",
+      hiragana: "す",
+      exemplos: [
+        {
+          palavra: "すみません",
+          traducao: "com licença",
+        },
+        {
+          palavra: "すし",
+          traducao: "sushi",
+        },
+        {
+          palavra: "すわる",
+          traducao: "sentar",
+        },
+      ],
+    },
+    se: {
+      letra: "se",
+      hiragana: "せ",
+      exemplos: [
+        {
+          palavra: "せんせい",
+          traducao: "professor",
+        },
+        {
+          palavra: "せかい",
+          traducao: "mundo",
+        },
+        {
+          palavra: "せん",
+          traducao: "linha",
+        },
+      ],
+    },
+    so: {
+      letra: "so",
+      hiragana: "そ",
+      exemplos: [
+        {
+          palavra: "そら",
+          traducao: "céu",
+        },
+        {
+          palavra: "そうじ",
+          traducao: "limpeza",
+        },
+        {
+          palavra: "そこ",
+          traducao: "ali",
+        },
+      ],
+    },
+    ta: {
+      letra: "ta",
+      hiragana: "た",
+      exemplos: [
+        {
+          palavra: "たべる",
+          traducao: "comer",
+        },
+        {
+          palavra: "たなか",
+          traducao: "Tanaka (nome próprio)",
+        },
+        {
+          palavra: "たまご",
+          traducao: "ovo",
+        },
+      ],
+    },
+    chi: {
+      letra: "chi",
+      hiragana: "ち",
+      exemplos: [
+        {
+          palavra: "ちいさい",
+          traducao: "pequeno",
+        },
+        {
+          palavra: "ちかん",
+          traducao: "agressor",
+        },
+        {
+          palavra: "ちち",
+          traducao: "pai",
+        },
+      ],
+    },
+    tsu: {
+      letra: "tsu",
+      hiragana: "つ",
+      exemplos: [
+        {
+          palavra: "つくえ",
+          traducao: "mesa",
+        },
+        {
+          palavra: "つめたい",
+          traducao: "frio (ao toque)",
+        },
+        {
+          palavra: "つかう",
+          traducao: "usar",
+        },
+      ],
+    },
+    te: {
+      letra: "te",
+      hiragana: "て",
+      exemplos: [
+        {
+          palavra: "てがみ",
+          traducao: "carta",
+        },
+        {
+          palavra: "てんき",
+          traducao: "tempo (clima)",
+        },
+        {
+          palavra: "てんし",
+          traducao: "anjo",
+        },
+      ],
+    },
+    to: {
+      letra: "to",
+      hiragana: "と",
+      exemplos: [
+        {
+          palavra: "ともだち",
+          traducao: "amigo",
+        },
+        {
+          palavra: "とおい",
+          traducao: "longe",
+        },
+        {
+          palavra: "とうきょう",
+          traducao: "Tóquio",
+        },
+      ],
+    },
+    na: {
+      letra: "na",
+      hiragana: "な",
+      exemplos: [
+        {
+          palavra: "なまえ",
+          traducao: "nome",
+        },
+        {
+          palavra: "なつ",
+          traducao: "verão",
+        },
+        {
+          palavra: "なか",
+          traducao: "dentro",
+        },
+      ],
+    },
+    ni: {
+      letra: "ni",
+      hiragana: "に",
+      exemplos: [
+        {
+          palavra: "にほん",
+          traducao: "Japão",
+        },
+        {
+          palavra: "にちようび",
+          traducao: "domingo",
+        },
+        {
+          palavra: "にわ",
+          traducao: "jardim",
+        },
+      ],
+    },
+    nu: {
+      letra: "nu",
+      hiragana: "ぬ",
+      exemplos: [
+        {
+          palavra: "ぬいぐるみ",
+          traducao: "pelúcia",
+        },
+        {
+          palavra: "ぬくもり",
+          traducao: "calor",
+        },
+        {
+          palavra: "ぬすむ",
+          traducao: "roubar",
+        },
+      ],
+    },
+    ne: {
+      letra: "ne",
+      hiragana: "ね",
+      exemplos: [
+        {
+          palavra: "ねこ",
+          traducao: "gato",
+        },
+        {
+          palavra: "ねむい",
+          traducao: "com sono",
+        },
+        {
+          palavra: "ねだん",
+          traducao: "preço",
+        },
+      ],
+    },
+    no: {
+      letra: "no",
+      hiragana: "の",
+      exemplos: [
+        {
+          palavra: "のみもの",
+          traducao: "bebida",
+        },
+        {
+          palavra: "のぼる",
+          traducao: "subir",
+        },
+        {
+          palavra: "のうりょく",
+          traducao: "habilidade",
+        },
+      ],
+    },
+    ha: {
+      letra: "ha",
+      hiragana: "は",
+      exemplos: [
+        {
+          palavra: "はな",
+          traducao: "flor",
+        },
+        {
+          palavra: "はし",
+          traducao: "hashi (palitos)",
+        },
+        {
+          palavra: "はいる",
+          traducao: "entrar",
+        },
+      ],
+    },
+    hi: {
+      letra: "hi",
+      hiragana: "ひ",
+      exemplos: [
+        {
+          palavra: "ひと",
+          traducao: "pessoa",
+        },
+        {
+          palavra: "ひる",
+          traducao: "tarde",
+        },
+        {
+          palavra: "ひこうき",
+          traducao: "avião",
+        },
+      ],
+    },
+    fu: {
+      letra: "fu",
+      hiragana: "ふ",
+      exemplos: [
+        {
+          palavra: "ふたり",
+          traducao: "duas pessoas",
+        },
+        {
+          palavra: "ふく",
+          traducao: "roupa",
+        },
+        {
+          palavra: "ふる",
+          traducao: "chover",
+        },
+      ],
+    },
+    he: {
+      letra: "he",
+      hiragana: "へ",
+      exemplos: [
+        {
+          palavra: "へや",
+          traducao: "quarto",
+        },
+        {
+          palavra: "へんじ",
+          traducao: "resposta",
+        },
+        {
+          palavra: "へいわ",
+          traducao: "paz",
+        },
+      ],
+    },
+    ho: {
+      letra: "ho",
+      hiragana: "ほ",
+      exemplos: [
+        {
+          palavra: "ほん",
+          traducao: "livro",
+        },
+        {
+          palavra: "ほし",
+          traducao: "estrela",
+        },
+        {
+          palavra: "ほうりつ",
+          traducao: "lei",
+        },
+      ],
+    },
+    ma: {
+      letra: "ma",
+      hiragana: "ま",
+      exemplos: [
+        {
+          palavra: "まど",
+          traducao: "janela",
+        },
+        {
+          palavra: "まるい",
+          traducao: "redondo",
+        },
+        {
+          palavra: "まち",
+          traducao: "cidade",
+        },
+      ],
+    },
+    mi: {
+      letra: "mi",
+      hiragana: "み",
+      exemplos: [
+        {
+          palavra: "みず",
+          traducao: "água",
+        },
+        {
+          palavra: "みせ",
+          traducao: "loja",
+        },
+        {
+          palavra: "みんな",
+          traducao: "todos",
+        },
+      ],
+    },
+    mu: {
+      letra: "mu",
+      hiragana: "む",
+      exemplos: [
+        {
+          palavra: "むし",
+          traducao: "inseto",
+        },
+        {
+          palavra: "むかし",
+          traducao: "antigamente",
+        },
+        {
+          palavra: "むずかしい",
+          traducao: "difícil",
+        },
+      ],
+    },
+    me: {
+      letra: "me",
+      hiragana: "め",
+      exemplos: [
+        {
+          palavra: "めがね",
+          traducao: "óculos",
+        },
+        {
+          palavra: "め",
+          traducao: "olho",
+        },
+        {
+          palavra: "めいわく",
+          traducao: "inconveniente",
+        },
+      ],
+    },
+    mo: {
+      letra: "mo",
+      hiragana: "も",
+      exemplos: [
+        {
+          palavra: "もの",
+          traducao: "coisa",
+        },
+        {
+          palavra: "もくてき",
+          traducao: "objetivo",
+        },
+        {
+          palavra: "もり",
+          traducao: "floresta",
+        },
+      ],
+    },
+    ya: {
+      letra: "ya",
+      hiragana: "や",
+      exemplos: [
+        {
+          palavra: "やま",
+          traducao: "montanha",
+        },
+        {
+          palavra: "やさい",
+          traducao: "vegetais",
+        },
+        {
+          palavra: "やすい",
+          traducao: "barato",
+        },
+      ],
+    },
+    yu: {
+      letra: "yu",
+      hiragana: "ゆ",
+      exemplos: [
+        {
+          palavra: "ゆうがた",
+          traducao: "tarde",
+        },
+        {
+          palavra: "ゆめ",
+          traducao: "sonho",
+        },
+        {
+          palavra: "ゆび",
+          traducao: "dedo",
+        },
+      ],
+    },
+    yo: {
+      letra: "yo",
+      hiragana: "よ",
+      exemplos: [
+        {
+          palavra: "よる",
+          traducao: "noite",
+        },
+        {
+          palavra: "よわい",
+          traducao: "fraco",
+        },
+        {
+          palavra: "よん",
+          traducao: "quatro",
+        },
+      ],
+    },
+    ra: {
+      letra: "ra",
+      hiragana: "ら",
+      exemplos: [
+        {
+          palavra: "らいねん",
+          traducao: "próximo ano",
+        },
+        {
+          palavra: "らいしゅう",
+          traducao: "próxima semana",
+        },
+        {
+          palavra: "らくがき",
+          traducao: "rascunho",
+        },
+      ],
+    },
+    ri: {
+      letra: "ri",
+      hiragana: "り",
+      exemplos: [
+        {
+          palavra: "りんご",
+          traducao: "maçã",
+        },
+        {
+          palavra: "りょうり",
+          traducao: "culinária",
+        },
+        {
+          palavra: "りゅうがく",
+          traducao: "estudar no exterior",
+        },
+      ],
+    },
+    ru: {
+      letra: "ru",
+      hiragana: "る",
+      exemplos: [
+        {
+          palavra: "るす",
+          traducao: "ficar fora de casa",
+        },
+        {
+          palavra: "るい",
+          traducao: "categoria",
+        },
+        {
+          palavra: "るん",
+          traducao: "regra",
+        },
+      ],
+    },
+    re: {
+      letra: "re",
+      hiragana: "れ",
+      exemplos: [
+        {
+          palavra: "れんしゅう",
+          traducao: "prática",
+        },
+        {
+          palavra: "れいぞうこ",
+          traducao: "geladeira",
+        },
+        {
+          palavra: "れんあい",
+          traducao: "amor",
+        },
+      ],
+    },
+    ro: {
+      letra: "ro",
+      hiragana: "ろ",
+      exemplos: [
+        {
+          palavra: "ろうか",
+          traducao: "corredor",
+        },
+        {
+          palavra: "ろっぴゃく",
+          traducao: "seiscentos",
+        },
+        {
+          palavra: "ろく",
+          traducao: "seis",
+        },
+      ],
+    },
+    wa: {
+      letra: "wa",
+      hiragana: "わ",
+      exemplos: [
+        {
+          palavra: "わたし",
+          traducao: "eu",
+        },
+        {
+          palavra: "わるい",
+          traducao: "ruim",
+        },
+        {
+          palavra: "わかる",
+          traducao: "entender",
+        },
+      ],
+    },
+    wo: {
+      letra: "wo",
+      hiragana: "を",
+      exemplos: [
+        {
+          palavra: "をたく",
+          traducao: "querer",
+        },
+        {
+          palavra: "をん",
+          traducao: "on (letra)",
+        },
+        {
+          palavra: "をり",
+          traducao: "guerreiro",
+        },
+      ],
+    },
+    n: {
+      letra: "n",
+      hiragana: "ん",
+      exemplos: [
+        {
+          palavra: "にほん",
+          traducao: "Japão",
+        },
+        {
+          palavra: "なまえ",
+          traducao: "nome",
+        },
+        {
+          palavra: "なん",
+          traducao: "o quê",
+        },
+      ],
+    },
+  },
+];
+
+export default hiraganaList;
