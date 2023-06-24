46d5c1bdc7410085ac3cc88614b0a7039b1844af
Karla Oshikawa <karla_hara@hotmail.com>
2023-06-24 10:24:19 +0900

    style: pag de como se preparar

--- JLPT-descomplicado/src/components/PreparingForJLPT.js
+++ JLPT-descomplicado/src/components/PreparingForJLPT.js
@@ -1,144 +1,149 @@
-import style from "./PreparingForJLPT.module";
+import style from "./PreparingForJLPT.module.scss"
 
 export default function PreparingForJLPT() {
   return (
     <div className={style.PreparingForJLPT_container}>
-      <h1>
-        Dicas para se preparar para o JLPT (Japanese Language Proficiency Test)
-      </h1>
+      <h1>Dicas para se preparar para o JLPT</h1>
       <p>
         Se você quer ter sucesso no JLPT, é essencial se preparar bem. Aqui
         estão algumas dicas valiosas para ajudá-lo a se preparar de forma eficaz
         para o exame.
       </p>
 
-      <h2>1- Conheça a estrutura do exame:</h2>
+      <div className={style.PreparingForJLPT_box}>
+        <h2>1- Conheça a estrutura do exame:</h2>
 
-      <p>
-        Antes de começar a estudar para o JLPT, é importante conhecer a sua
-        estrutura. O exame é dividido em diferentes níveis, do N5 (o mais
-        básico) ao N1 (o mais avançado), e cada nível tem seções específicas
-        para vocabulário, gramática, leitura e audição. Compreender a estrutura
-        do exame ajudará você a direcionar seus estudos e a se sentir mais
-        confiante durante a prova. Por isso, dedique um tempo para se
-        familiarizar com a estrutura do JLPT antes de começar a estudar.
-      </p>
+        <p>
+          Antes de começar a estudar para o JLPT, é importante conhecer a sua
+          estrutura. O exame é dividido em diferentes níveis, do N5 (o mais
+          básico) ao N1 (o mais avançado), e cada nível tem seções específicas
+          para vocabulário, gramática, leitura e audição. Compreender a
+          estrutura do exame ajudará você a direcionar seus estudos e a se
+          sentir mais confiante durante a prova. Por isso, dedique um tempo para
+          se familiarizar com a estrutura do JLPT antes de começar a estudar.
+        </p>
 
-      <h2>2- Estabeleça um cronograma de estudos:</h2>
-      <p>
-        Para se preparar adequadamente para o JLPT, é essencial criar um
-        cronograma de estudos. Reserve um tempo diário ou semanal para estudar
-        vocabulário, gramática, leitura e audição, dependendo das áreas em que
-        você precisa se concentrar. Para não se sobrecarregar, divida o material
-        em partes gerenciáveis e estabeleça metas realistas para acompanhar seu
-        progresso. Dessa forma, você poderá se preparar de forma consistente e
-        eficaz para o exame.
-      </p>
+        <h2>2- Estabeleça um cronograma de estudos:</h2>
+        <p>
+          Para se preparar adequadamente para o JLPT, é essencial criar um
+          cronograma de estudos. Reserve um tempo diário ou semanal para estudar
+          vocabulário, gramática, leitura e audição, dependendo das áreas em que
+          você precisa se concentrar. Para não se sobrecarregar, divida o
+          material em partes gerenciáveis e estabeleça metas realistas para
+          acompanhar seu progresso. Dessa forma, você poderá se preparar de
+          forma consistente e eficaz para o exame.
+        </p>
 
-      <h2>3- Use materiais de estudo apropriados:</h2>
-      <p>
-        Há muitos livros, recursos online e aplicativos disponíveis para ajudar
-        na preparação para o JLPT. Aqui no JLPT - Descomplicado, você encontra
-        um vasto material de estudo e estamos sempre adicionando novas
-        funcionalidades. É importante escolher materiais que sejam adequados ao
-        seu nível de proficiência. Livros específicos para cada nível do JLPT
-        são uma opção popular, pois geralmente contêm exercícios de prática e
-        exemplos de perguntas semelhantes às do exame real. Portanto, pesquise e
-        escolha os materiais de estudo que melhor se adequem às suas
-        necessidades e estilo de aprendizagem.
-      </p>
+        <h2>3- Use materiais de estudo apropriados:</h2>
+        <p>
+          Há muitos livros, recursos online e aplicativos disponíveis para
+          ajudar na preparação para o JLPT. Aqui no JLPT - Descomplicado, você
+          encontra um vasto material de estudo e estamos sempre adicionando
+          novas funcionalidades. É importante escolher materiais que sejam
+          adequados ao seu nível de proficiência. Livros específicos para cada
+          nível do JLPT são uma opção popular, pois geralmente contêm exercícios
+          de prática e exemplos de perguntas semelhantes às do exame real.
+          Portanto, pesquise e escolha os materiais de estudo que melhor se
+          adequem às suas necessidades e estilo de aprendizagem.
+        </p>
 
-      <h2>4- Pratique a leitura intensiva:</h2>
-      <p>
-        A seção de leitura do JLPT requer habilidades de compreensão de texto em
-        japonês. Para aprimorar sua capacidade de entender e extrair
-        informações, é recomendável que você leia regularmente artigos, notícias
-        ou livros em japonês. Durante a leitura, faça anotações de vocabulário e
-        gramática desconhecidos e revise-os regularmente. Dessa forma, você
-        poderá expandir seu vocabulário e melhorar sua compreensão da língua
-        japonesa, o que será muito útil durante a prova do JLPT. Portanto,
-        reserve um tempo para ler regularmente em japonês e pratique suas
-        habilidades de compreensão de texto.
-      </p>
+        <h2>4- Pratique a leitura intensiva:</h2>
+        <p>
+          A seção de leitura do JLPT requer habilidades de compreensão de texto
+          em japonês. Para aprimorar sua capacidade de entender e extrair
+          informações, é recomendável que você leia regularmente artigos,
+          notícias ou livros em japonês. Durante a leitura, faça anotações de
+          vocabulário e gramática desconhecidos e revise-os regularmente. Dessa
+          forma, você poderá expandir seu vocabulário e melhorar sua compreensão
+          da língua japonesa, o que será muito útil durante a prova do JLPT.
+          Portanto, reserve um tempo para ler regularmente em japonês e pratique
+          suas habilidades de compreensão de texto.
+        </p>
 
-      <h2>5- Ouça e pratique a audição:</h2>
-      <p>
-        A seção de audição do JLPT requer que você entenda diálogos e monólogos
-        em japonês. Para se preparar adequadamente, é recomendável que você ouça
-        regularmente podcasts, programas de rádio, músicas e materiais de áudio
-        em japonês para se familiarizar com diferentes sotaques e velocidades de
-        fala. Além disso, pratique responder a perguntas de compreensão auditiva
-        para ganhar confiança nessa habilidade. Dessa forma, você poderá se
-        sentir mais confortável durante a seção de audição do JLPT e terá mais
-        chances de sucesso na prova. Portanto, dedique um tempo para praticar
-        sua habilidade de compreensão auditiva em japonês.
-      </p>
+        <h2>5- Ouça e pratique a audição:</h2>
+        <p>
+          A seção de audição do JLPT requer que você entenda diálogos e
+          monólogos em japonês. Para se preparar adequadamente, é recomendável
+          que você ouça regularmente podcasts, programas de rádio, músicas e
+          materiais de áudio em japonês para se familiarizar com diferentes
+          sotaques e velocidades de fala. Além disso, pratique responder a
+          perguntas de compreensão auditiva para ganhar confiança nessa
+          habilidade. Dessa forma, você poderá se sentir mais confortável
+          durante a seção de audição do JLPT e terá mais chances de sucesso na
+          prova. Portanto, dedique um tempo para praticar sua habilidade de
+          compreensão auditiva em japonês.
+        </p>
 
-      <h2>6- Faça simulados do JLPT:</h2>
-      <p>
-        Fazer simulados do JLPT é uma maneira eficaz de se familiarizar com o
-        formato e o tempo de cada seção do exame. Para isso, você pode procurar
-        por livros de simulados ou sites que ofereçam testes práticos. Durante a
-        realização dos simulados, é importante observar seu desempenho,
-        identificar suas áreas de fraqueza e concentrar-se em melhorá-las. Dessa
-        forma, você poderá treinar suas habilidades e se sentir mais confiante
-        durante a prova do JLPT. Portanto, reserve um tempo para fazer simulados
-        e use-os como uma ferramenta de preparação para o exame.
-      </p>
+        <h2>6- Faça simulados do JLPT:</h2>
+        <p>
+          Fazer simulados do JLPT é uma maneira eficaz de se familiarizar com o
+          formato e o tempo de cada seção do exame. Para isso, você pode
+          procurar por livros de simulados ou sites que ofereçam testes
+          práticos. Durante a realização dos simulados, é importante observar
+          seu desempenho, identificar suas áreas de fraqueza e concentrar-se em
+          melhorá-las. Dessa forma, você poderá treinar suas habilidades e se
+          sentir mais confiante durante a prova do JLPT. Portanto, reserve um
+          tempo para fazer simulados e use-os como uma ferramenta de preparação
+          para o exame.
+        </p>
 
-      <h2>7- Pratique a escrita:</h2>
-      <p>
-        Embora a seção de escrita não esteja incluída no JLPT, é uma habilidade
-        importante a ser desenvolvida para aprofundar seu domínio do idioma
-        japonês. Para isso, é recomendável que você pratique escrever textos em
-        japonês, desde parágrafos simples até ensaios mais complexos. Além
-        disso, é importante que você peça a um professor nativo de japonês ou a
-        um falante fluente para revisar e corrigir seus escritos. Dessa forma,
-        você poderá receber feedback valioso e aprimorar sua habilidade de
-        escrita em japonês. Portanto, dedique um tempo para praticar sua escrita
-        em japonês e busque a ajuda de um profissional para melhorar ainda mais.
-      </p>
+        <h2>7- Pratique a escrita:</h2>
+        <p>
+          Embora a seção de escrita não esteja incluída no JLPT, é uma
+          habilidade importante a ser desenvolvida para aprofundar seu domínio
+          do idioma japonês. Para isso, é recomendável que você pratique
+          escrever textos em japonês, desde parágrafos simples até ensaios mais
+          complexos. Além disso, é importante que você peça a um professor
+          nativo de japonês ou a um falante fluente para revisar e corrigir seus
+          escritos. Dessa forma, você poderá receber feedback valioso e
+          aprimorar sua habilidade de escrita em japonês. Portanto, dedique um
+          tempo para praticar sua escrita em japonês e busque a ajuda de um
+          profissional para melhorar ainda mais.
+        </p>
 
-      <h2>8- Participe de aulas ou grupos de estudo:</h2>
-      <p>
-        Se possível, participar de aulas de japonês ou grupos de estudo é uma
-        ótima maneira de interagir com outros estudantes e praticar conversação
-        em japonês. Durante as aulas ou grupos de estudo, você pode discutir
-        tópicos em japonês e receber feedback dos outros estudantes e do
-        professor, o que ajudará a fortalecer suas habilidades de comunicação.
-        Além disso, você pode ter a oportunidade de praticar gramática,
-        vocabulário e pronúncia em um ambiente seguro e orientado. Portanto,
-        considere participar de aulas de japonês ou grupos de estudo para
-        aprimorar suas habilidades de comunicação em japonês.
-      </p>
+        <h2>8- Participe de aulas ou grupos de estudo:</h2>
+        <p>
+          Se possível, participar de aulas de japonês ou grupos de estudo é uma
+          ótima maneira de interagir com outros estudantes e praticar
+          conversação em japonês. Durante as aulas ou grupos de estudo, você
+          pode discutir tópicos em japonês e receber feedback dos outros
+          estudantes e do professor, o que ajudará a fortalecer suas habilidades
+          de comunicação. Além disso, você pode ter a oportunidade de praticar
+          gramática, vocabulário e pronúncia em um ambiente seguro e orientado.
+          Portanto, considere participar de aulas de japonês ou grupos de estudo
+          para aprimorar suas habilidades de comunicação em japonês.
+        </p>
 
-      <h2>9- Tempo de estudo para cada fase:</h2>
-      <p>
-        O tempo necessário para se preparar para cada nível do JLPT pode variar
-        dependendo do nível atual de proficiência em japonês e da quantidade de
-        tempo que pode ser dedicada aos estudos. No entanto, em média,
-        recomenda-se que os candidatos estudem por cerca de 300 a 400 horas para
-        se preparar para o N5, o nível mais básico do exame. Para o N4, é
-        recomendável estudar por cerca de 500 a 600 horas. Já para os níveis N3
-        e N2, é necessário estudar por aproximadamente 700 a 800 horas. Por fim,
-        para o nível mais avançado, o N1, é recomendável estudar por pelo menos
-        900 a 1.000 horas. É importante ressaltar que esses números são apenas
-        uma estimativa e que o tempo necessário para se preparar pode variar
-        dependendo do indivíduo e de seus objetivos pessoais.
-      </p>
+        <h2>9- Tempo de estudo para cada fase:</h2>
+        <p>
+          O tempo necessário para se preparar para cada nível do JLPT pode
+          variar dependendo do nível atual de proficiência em japonês e da
+          quantidade de tempo que pode ser dedicada aos estudos. No entanto, em
+          média, recomenda-se que os candidatos estudem por cerca de 300 a 400
+          horas para se preparar para o N5, o nível mais básico do exame. Para o
+          N4, é recomendável estudar por cerca de 500 a 600 horas. Já para os
+          níveis N3 e N2, é necessário estudar por aproximadamente 700 a 800
+          horas. Por fim, para o nível mais avançado, o N1, é recomendável
+          estudar por pelo menos 900 a 1.000 horas. É importante ressaltar que
+          esses números são apenas uma estimativa e que o tempo necessário para
+          se preparar pode variar dependendo do indivíduo e de seus objetivos
+          pessoais.
+        </p>
 
-      <h2>10- O que fazer no dia da prova:</h2>
-      <p>
-        No dia da prova JLPT, é importante estar bem preparado tanto mental
-        quanto fisicamente. Uma dica importante é chegar ao local da prova com
-        antecedência para evitar imprevistos e ter tempo suficiente para se
-        acalmar e se concentrar. É recomendado também levar uma garrafa de água.
-        Além disso, é essencial ler cuidadosamente as instruções da prova antes
-        de começar e gerenciar bem o tempo disponível para cada seção. É normal
-        sentir um pouco de nervosismo, mas é importante manter a calma e a
-        confiança em suas habilidades. Lembre-se de que esta é uma oportunidade
-        para testar seu conhecimento e habilidades em japonês. Boa sorte!
-      </p>
+        <h2>10- O que fazer no dia da prova:</h2>
+        <p>
+          No dia da prova JLPT, é importante estar bem preparado tanto mental
+          quanto fisicamente. Uma dica importante é chegar ao local da prova com
+          antecedência para evitar imprevistos e ter tempo suficiente para se
+          acalmar e se concentrar. É recomendado também levar uma garrafa de
+          água. Além disso, é essencial ler cuidadosamente as instruções da
+          prova antes de começar e gerenciar bem o tempo disponível para cada
+          seção. É normal sentir um pouco de nervosismo, mas é importante manter
+          a calma e a confiança em suas habilidades. Lembre-se de que esta é uma
+          oportunidade para testar seu conhecimento e habilidades em japonês.
+          Boa sorte!
+        </p>
+      </div>
 
       <p>
         Concluindo, preparar-se para o JLPT requer dedicação e um plano de
