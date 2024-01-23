4265d34c9f1bf9c68b440cd2de8d0a76ffa3c510
Karla Oshikawa <karla_hara@hotmail.com>
2024-01-23 14:49:33 +0900

    feat: date update

--- JLPT-descomplicado/src/components/TestDate.js
+++ JLPT-descomplicado/src/components/TestDate.js
@@ -3,10 +3,11 @@ import style from "./TestDate.module.scss";
 export default function TestDate() {
   return (
     <div className={style.testDate_container}>
-      <p>Data do teste JLPT em 2023</p>
+      <p>Data do teste JLPT em 2024</p>
       <div className={style.testDate_box}>
-        <p>dia 2 de Julho(domingo)</p>
-        <p>dia 3 de Dezembro(domingo)</p>
+        <p>a data ainda não foi definida</p>
+        {/* <p>dia 2 de Julho(domingo)</p> */}
+        {/* <p>dia 3 de Dezembro(domingo)</p> */}
       </div>
     </div>
   );
